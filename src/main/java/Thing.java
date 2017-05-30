793a1fcf32b30a9ae2520a8ea6af133104126b7b
CraigPerm <craigl@bcommunications.com.au>
2017-05-30 08:51:00 +0800

    Update Thing.java

--- src/main/java/Thing.java
+++ src/main/java/Thing.java
@@ -3,8 +3,5 @@
 public class Thing {
     // thing update release-0.0.12 branch (0.0.12-SNAPSHOT)
     // another update to same branch
-<<<<<<< HEAD
-=======
     // normal update on 0.0.13-SNAPSHOT
->>>>>>> refs/heads/release-0.0.14
 }
